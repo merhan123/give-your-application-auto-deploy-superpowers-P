7e7ff0067b39c814ec831f75569c23c82dc695b4
mira adel <merhan.adel.1@gmail.com>
2022-07-20 21:57:43 +0200

    local migration phase

---
+++ backend/src/migrations/1658345032067-UserFullName.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserFullName1658345032067 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
