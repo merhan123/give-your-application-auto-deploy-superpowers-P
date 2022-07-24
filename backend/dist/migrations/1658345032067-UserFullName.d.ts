41ce542d4c0b2066356919f5949de2ec51cc76c9
mira adel <merhan.adel.1@gmail.com>
2022-07-24 14:17:20 +0200

    screenshot5 commit

--- backend/dist/migrations/1658345032067-UserFullName.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class UserFullName1658345032067 implements MigrationInterface {
-    up(queryRunner: QueryRunner): Promise<any>;
-    down(queryRunner: QueryRunner): Promise<any>;
-}
